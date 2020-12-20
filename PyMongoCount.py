b89e7a1f85207e4db27f788a95bdddb3b19f8ae1
Abhishek <abhimady13@gmail.com>
2020-12-20 19:12:08 +0530

    Count Example

---
+++ PyMongoCount.py
@@ -0,0 +1,11 @@
+import pymongo
+from pymongo import MongoClient
+
+cluster = MongoClient("mongodb://localhost:27017")
+
+db = cluster["school"]
+collection = db["students"]
+
+results = collection.count_documents({})
+
+print(results)
